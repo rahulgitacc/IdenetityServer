c37e6f783f7dfa23b4c38db38847156061a0af8e
Rahul Sen <rsen253@hotmail.com>
2018-09-16 20:54:09 +0530

    Authentication using azure active directory

---
+++ AspNetCore.Auth.Web/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AspNetCore.Auth.Web.Controllers
+{
+    [Route("auth")]
+    public class AuthController : Controller
+    {
+        [Route("signin")]
+        public IActionResult SignIn()
+        {
+            return Challenge(new AuthenticationProperties { RedirectUri = "/" });
+        }
+
+        [Route("signout")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task SignOut()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+
+        }
+    }
+}
--- AspNetCore.Auth.Web/Startup.cs
+++ AspNetCore.Auth.Web/Startup.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 namespace AspNetCore.Auth.Web
 {
@@ -24,7 +27,29 @@ namespace AspNetCore.Auth.Web
                 // ensure that the application should use HTTPS
                 options.Filters.Add(new RequireHttpsAttribute());
             });
-
+            services.AddAuthentication(option =>
+            {
+                // tell the system to use open id connect by default when we need to authenticate any user
+                option.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                // use for actual signin
+                option.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                // tells system to authenticate incoming request using cookie authentication
+                option.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            })
+                .AddOpenIdConnect(options =>
+                {
+                    //tells the system to use open id connect endpoint which is given below
+                    options.Authority = "https://login.microsoftonline.com/rahulidentitydemo.onmicrosoft.com";
+                    // setting the name of the application
+                    options.ClientId = "a0992cf3-4cb3-4ae9-ad53-5f841db62dc7";
+                    // setting response type
+                    options.ResponseType = OpenIdConnectResponseType.IdToken;
+                    //callback path/ reply url
+                    options.CallbackPath = "/auth/signin-callback";
+                    options.SignedOutRedirectUri = "https://localhost:44341/";
+                    options.TokenValidationParameters.NameClaimType = "name";
+                })
+                .AddCookie();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -45,7 +70,7 @@ namespace AspNetCore.Auth.Web
             // Rewrite rule for redirect the user to https if anyone request for http
             app.UseRewriter(new RewriteOptions().AddRedirectToHttps(301, 44341));
             app.UseStaticFiles();
-
+            app.UseAuthentication();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
